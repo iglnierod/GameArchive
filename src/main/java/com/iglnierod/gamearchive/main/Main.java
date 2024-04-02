80b026b6fb5da2930d6c5c2815262356e17975db
Rodrigo Iglesias <iglesias.nieto.rodrigo.ald@gmail.com>
2024-04-02 15:55:41 +0200

    Proyecto base

---
+++ src/main/java/com/iglnierod/gamearchive/main/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.iglnierod.gamearchive.main;
+
+/**
+ *
+ * @author rodri
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
